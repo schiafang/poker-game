28f25cea770cefebf5dd1d32473eb86766b87ee2
carey <schiafang@gmail.com>
2020-03-26 22:38:32 +0800

    score and times

--- card.js
+++ card.js
@@ -12,17 +12,10 @@ const Symbols = [
   'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
   'https://image.flaticon.com/icons/svg/105/105219.svg'  // 梅花
 ]
+
 //----- 模組 view -----//
 const view = {
-  // getCardFront()負責正面卡片內容
-  // displayCards()找出容器，抽換內容
-  // 產生52個數字陣列，迭代陣列放進displayCards()
-  // 將52張牌綁上監聽
-  // 將背面與正面分開處理，加入getCardBack()，加上dataset-*
-  // displayCards()預設內容改為背面
-  // 加入翻牌動作的函式flipCard()，在監聽器上加上翻牌函式
   getCardFront(index) {
-    //卡牌數字是 index 除以 13後的「餘數 +1」
     const number = this.transformNumber((index % 13) + 1)
     const symbol = Symbols[Math.floor(index / 13)]
     return `
@@ -58,26 +51,41 @@ const view = {
     //帶入參數改由controller呼叫洗牌模組
     rootElement.innerHTML = indexes.map(index => this.getCardBack(index)).join('')
   },
-  flipCard(card) {
-    if (card.classList.contains('back')) {
-      card.classList.remove('back')
-      card.innerHTML = this.getCardFront(Number(card.dataset.index)) //HTML回傳的是字串
-      return
-    }
-    card.classList.add('back')
-    card.innerHTML = null
+  flipCards(...cards) {
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        card.classList.remove('back')
+        card.innerHTML = this.getCardFront(Number(card.dataset.index)) //HTML回傳的是字串
+        return
+      }
+      card.classList.add('back')
+      card.innerHTML = null
+    })
   },
-  pairCard(card) {
-    card.classList.add('paired')
+  pairCards(...cards) {
+    cards.map(card => {
+      card.classList.add('paired')
+    })
+  },
+  renderScore(score) {
+    document.querySelector(".score").innerText = `Score: ${score}`;
+  },
+
+  renderTriedTimes(times) {
+    document.querySelector(".tried").innerText = `You've tried: ${times} times`;
   }
 }
+
 //----- 模組 model-----//
 const model = {
   revealedCards: [],
   isRevealedCardsMatched() {
     return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13 //回傳布林值
-  }
+  },
+  score: 0,
+  triedTimes: 0
 }
+
 //----- 模組 controller -----//
 const controller = {
   //目前遊戲狀態
@@ -93,12 +101,13 @@ const controller = {
     //操作狀態轉換
     switch (this.currentState) {
       case GAME_STATE.FirstCardAwaits:
-        view.flipCard(card)
+        view.renderTriedTimes(++model.triedTimes) //翻牌次數
+        view.flipCards(card)
         model.revealedCards.push(card)
         this.currentState = GAME_STATE.SecondCardAwaits
         break
       case GAME_STATE.SecondCardAwaits:
-        view.flipCard(card)
+        view.flipCards(card)
         model.revealedCards.push(card)
 
         if (model.isRevealedCardsMatched()) {
@@ -106,28 +115,27 @@ const controller = {
           //配對成功，並加上配對成功的pairCard樣式
           //清空暫存牌卡區並回到等待第一張牌狀態
           this.currentState = GAME_STATE.CardsMatched
-          view.pairCard(model.revealedCards[0])
-          view.pairCard(model.revealedCards[1])
+          view.renderScore(model.score += 10) //加分數
+          view.pairCards(...model.revealedCards)
           model.revealedCards = []
           this.currentState = GAME_STATE.FirstCardAwaits
         } else {
           //配對失敗，進入配對失敗狀態
           //呼叫flipCard將牌翻回正面清空暫存區並回到等待第一張牌狀態
           this.currentState = GAME_STATE.CardsMatchFailed
-          setTimeout(() => {
-            view.flipCard(model.revealedCards[0])
-            view.flipCard(model.revealedCards[1])
-            model.revealedCards = []
-          }, 500)
+          setTimeout(this.resetCards, 500)
           this.currentState = GAME_STATE.FirstCardAwaits
         }
         break
     }
-    console.log(model.revealedCards)
-    console.log('this.currentState', this.currentState)
-    console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
-    //console.log(model.revealedCards[0].dataset.index)
-    console.log(this.currentState)
+    // console.log(model.revealedCards)
+    // console.log('this.currentState', this.currentState)
+    // console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
+    // console.log(this.currentState)
+  },
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
   }
 }
 
@@ -142,16 +150,14 @@ const utility = {
     return number
   }
 }
-//view.displayCards()
+
+//由controller派發初始狀態畫面
 controller.generateCards()
 
 //----- 監聽 -----//
 document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('click', event => {
-    //view.flipCard(card)
-    //監聽使用者點擊卡片後遊戲推進的狀態
     controller.dispatchCardAction(card)
-
   })
 })
 
